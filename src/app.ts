4183e4459eaaa869441d9f47b50be4f8600a073c
Harry Craven <harrycraven.dev@gmail.com>
2021-12-31 11:45:38 +0000

    App and gitignore update.

--- src/app.ts
+++ src/app.ts
@@ -7,33 +7,43 @@ import cors from "cors";
 import helmet from "helmet";
 import compression from "compression";
 import config from "config";
+import mongoose from "mongoose";
 
 const run = async () => {
 	const app = express();
 
-	app.use(cors());
+	try {
+		console.log("DB connection string: ", config.get("db.connection"));
 
-	app.use(helmet());
+		const conn = await mongoose.connect(config.get("db.connection"));
 
-	app.use(compression());
+		app.use(cors());
 
-	// Use body parser to read sent json payloads
-	app.use(
-		bodyParser.urlencoded({
-			extended: true,
-		})
-	);
+		app.use(helmet());
 
-	app.use(bodyParser.json());
+		app.use(compression());
 
-	RegisterRoutes(app);
+		// Use body parser to read sent json payloads
+		app.use(
+			bodyParser.urlencoded({
+				extended: true,
+			})
+		);
+
+		app.use(bodyParser.json());
+
+		RegisterRoutes(app);
+	} catch (error) {
+		console.log("Process exited with error: ", error);
+		process.exit(1);
+	}
 
 	app.use(errorHandler);
 
 	const port = config.get("app.port") || 3000;
 
 	app.listen(port, () =>
-		console.log(`Example app listening at http://localhost:${port} `)
+		console.log(`Example app listening at http://localhost:${port} 🥳`)
 	);
 };
 
