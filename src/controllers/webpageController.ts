cf76ce62cecbff8b3d9c5c1fd6a1fb86ba6950b9
Harry Craven <harrycraven.dev@gmail.com>
2021-12-31 14:15:29 +0000

    Added all routes into a single file with an updated route name.

--- src/controllers/webpageController.ts
+++ src/controllers/webpageController.ts
@@ -1,11 +1,28 @@
 // src/users/usersController.ts
-import { Body, Controller, Post, Route, SuccessResponse } from "tsoa";
+import { Body, Controller, Post, Get, Route, SuccessResponse } from "tsoa";
 import { WebpageService } from "../services/webpageService";
+import { ExperienceService } from "../services/experienceService";
+import { MessageService } from "../services/messageService";
+import { MessageDto } from "../dtos/message.dto";
 
-@Route("subscribe")
+@Route("api/v1")
 export class WebpageController extends Controller {
-	@SuccessResponse("201", "Created")
-	@Post()
+	@SuccessResponse("201", "Get experience.") // Custom success response
+	@Get("experience")
+	public async getExperience(): Promise<any> {
+		this.setStatus(201); // set return status 201
+
+		const res = await new ExperienceService().getExperience();
+
+		if (!res.success) {
+			return res;
+		}
+
+		return res;
+	}
+
+	@SuccessResponse("201", "Subscription created.")
+	@Post("subscribe")
 	public async createEmailSubscription(
 		@Body() body: { email: string }
 	): Promise<any> {
@@ -18,6 +35,16 @@ export class WebpageController extends Controller {
 			this.setStatus(400);
 			return res;
 		}
+
+		return res;
+	}
+
+	@SuccessResponse("201", "Message successfully sent.")
+	@Post("message")
+	public async createMessage(@Body() requestBody: MessageDto): Promise<any> {
+		this.setStatus(201); // set return status 201
+
+		const res = await new MessageService().createMessage(requestBody);
 
 		return res;
 	}
